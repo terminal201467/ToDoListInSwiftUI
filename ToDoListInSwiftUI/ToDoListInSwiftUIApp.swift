ede4f1926ca89788c900a6b9a476f945a3c8b544
MJ <mj.mu@bionime.com>
2022-06-21 17:06:49 +0800

    Initial Commit

---
+++ ToDoListInSwiftUI/ToDoListInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListInSwiftUIApp.swift
+//  ToDoListInSwiftUI
+//
+//  Created by 辴 穆 on 2022/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
